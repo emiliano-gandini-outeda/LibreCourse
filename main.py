5659baa6d2edb6428e9ab539adc1191bea50e077
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-07-14 16:28:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from db import engine, Base
 from models import Usuario, Curso, Leccion, Nota 
 from routers import categories, auth_routers
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -14,3 +16,7 @@ app.include_router(auth_routers.router)
 @app.get("/health", tags=["health"])
 def health_check():
     return {"status": "ok", "message": "API is running smoothly"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
