fa5152b5819674d4cc6fca7d592fcc9643b13425
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-08-03 23:50:35 -0300

    Update settings.py

--- CourseTrackr/settings.py
+++ CourseTrackr/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-#k^n@^qs_g-$m#j^$^#*gqz4-rt1w16#$rl@4*pbi-@h#dndp+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["coursetrackr-production-dc46.up.railway.app"]
 
 
 # Application definition
