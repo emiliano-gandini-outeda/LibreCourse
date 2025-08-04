601fec740e973369b73cc31401838c4f6f68f6cd
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-08-03 23:24:13 -0300

    database URL change

--- CourseTrackr/settings.py
+++ CourseTrackr/settings.py
@@ -85,14 +85,8 @@ WSGI_APPLICATION = 'CourseTrackr.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': env.db() # uses DATABASE_URL
+    
 }
 
 
