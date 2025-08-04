f48038fac3b61b2c8c094643ee05b7616fc6c65a
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-08-03 23:07:49 -0300

    Course list

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_home, name='course-home'),
+    path('', views.course_list, name='course-home'),
     path('<int:id>/', views.course_page, name = "course-detail")
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.utils import timezone
 from django.conf import settings
 from .models import Course
-# Create your views here.
 from django.http import HttpResponse
+
+# Create your views here.
 
 print("TEMPLATE DIRS:", settings.TEMPLATES[0]['DIRS'])
 
@@ -11,5 +12,14 @@ def course_home(request):
         return render(request, 'courses/home.html')
     
 def course_page(request, id):
+        tzname = request.COOKIES.get("user_timezone")
+        
+        if tzname:
+            timezone.activate(tzname)
+        
         course = get_object_or_404(Course, id = id)
         return render(request, "courses/course_detail.html", {"course" : course})
+    
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, "courses/home.html", {"courses" : courses})
