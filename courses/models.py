538c13ee2bc1722b671f1f6094289228ee26d0af
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-08-26 22:04:43 -0300

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from users.models import CustomUser
+
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=450)
     createdDate = models.DateTimeField(auto_now_add=True)
-    
+    creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="created_courses")
+    status = models.CharField(max_length=15, choices=[("pub", "Public"), ("priv", "Private"), ("dra", "Draft")])
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = tags = models.ManyToManyField(Tag, blank=True, related_name="courses")
     def __str__(self):
         return f"#{self.id} : {self.title}"
+    
