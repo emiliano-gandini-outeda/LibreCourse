7b5aa1ecaa0d9e259b9a937bc9b2ec08a7fcc782
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-09-04 13:52:10 -0300

    Added missing slash

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.listUsers, name = "users"),
     path("login/", views.login_view, name = "login"),
     path("signup/", views.signup_view, name = "signup"),
-    path("logout", views.logout_view, name = "logout"),
+    path("logout/", views.logout_view, name = "logout"),
     path("user/<int:id>/", views.userDetails, name="user-details"),
 ]
