bbc1837153214d9fd55605edefae2a8cfb035167
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-05 01:11:06 -0300

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.publishedCourses, name = "list_courses"),
+    path("", views.publishedCourses, name = "courses"),
     path("<int:course_id>/", views.courseById, name = "course_details"),
     path("create/", views.createCourse, name = "create_course"),
     path("<int:course_id>/update/", views.updateCourse, name = "update_course"),
