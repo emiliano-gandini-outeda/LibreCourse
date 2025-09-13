bdd9f30dda8bee99d1a28bd6ff56d938d6fdb87c
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-13 12:20:53 -0300

    Create backends.py

---
+++ users/backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import BaseBackend
+from .models import User
+
+class EmailBackend(BaseBackend):
+ 
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        if email is None or password is None:
+            return None
+        try:
+            user = User.objects.get(email__iexact=email)
+        except User.DoesNotExist:
+            return None
+        
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        return user if self.user_can_authenticate(user) else None
+
+    def user_can_authenticate(self, user):
+        return user.is_active
