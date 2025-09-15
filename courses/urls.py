6ce1a9d5fc1dea5e50e04eb2d6062f8b682dc8fb
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-15 13:57:14 -0300

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.publishedCourses, name = "courses"),
-    path("<int:course_id>/", views.courseById, name = "course_details"),
-    path("create/", views.createCourse, name = "create_course"),
-    path("<int:course_id>/update/", views.updateCourse, name = "update_course"),
-    path("<int:course_id>/delete/", views.deleteCourse, name = "delete_course"),
+    path("", views.CourseListView.as_view(), name="course-list"),
+    path("create/", views.CourseCreateView.as_view(), name="course-create"),
+    path("<int:pk>/", views.CourseDetailView.as_view(), name="course-detail"),
+    path("<int:pk>/update/", views.CourseUpdateView.as_view(), name="course-update"),
+    path("<int:pk>/delete/", views.CourseDeleteView.as_view(), name="course-delete"),
+    path("<int:course_id>/lessons/create/", views.LessonCreateView.as_view(), name="lesson-create"),
+    path("<int:course_id>/lessons/<int:lesson_id>/update/", views.LessonUpdateView.as_view(), name="lesson-update"),
+    path("<int:course_id>/lessons/<int:lesson_id>/delete/", views.LessonDeleteView.as_view(), name="lesson-delete"),
+    path("<int:pk>/collaborators/manage/", views.ManageCollaboratorsView.as_view(), name="course-manage-collaborators"),
 ]
