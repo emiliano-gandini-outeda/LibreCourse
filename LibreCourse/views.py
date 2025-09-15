44431c4965584da089339de09fc59d5b533cfcd4
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-15 16:53:01 -0300

    permission denied view

--- LibreCourse/views.py
+++ LibreCourse/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 def home_view(request):
     return render(request, "main.html")
+
+
+def permission_denied_view(request, exception=None):
+    return render(request, "main/no-access", status=403)
