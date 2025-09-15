b34a8017ef606a6e2d3ca24378488a8e29b0b70f
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-15 16:52:36 -0300

    Fixed login redirect

--- LibreCourse/settings.py
+++ LibreCourse/settings.py
@@ -182,7 +182,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login/Logout URLs
-LOGIN_URL = '/login/'
+LOGIN_URL = 'users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
