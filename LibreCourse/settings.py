85c9971ff164a14b51f7086c86491ce9405677a5
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-10-03 01:41:09 -0300

    Update settings.py

--- LibreCourse/settings.py
+++ LibreCourse/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv("LOCAL_HOST"), "127.0.0.1" ]
+ALLOWED_HOSTS = [os.getenv("LOCAL_HOST"), "127.0.0.1", "*"]
 
 
 # Application definition
